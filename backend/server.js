dc8cfd786d27431e9971fc13054df799ffb973eb
vaibhavpatidar522 <vaibhavpatidar522@gmail.com>
2023-06-02 19:32:02 +0530

    MongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware.js');
+const connectDB = require('./config/db.js');
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
